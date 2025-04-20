66e92707d028221bab18ea41bb1f69c03c95a93e
mellw0101 <melwivensson@gmail.com>
2025-04-20 19:36:00 +0200

    No msg

--- amdgpu_pp_service.sh
+++ amdgpu_pp_service.sh
@@ -10,9 +10,10 @@ check_status() {
   local fail="$2"
   local text="$3"
   if [ $? -eq 0 ]; then
-    log "Successfully $succ $text"
+    log "Successfully $succ$text"
   else
-    log "Failed to $fail $text"
+    log "Failed to $fail$text"
+    exit 1
   fi
 }
 
@@ -23,9 +24,9 @@ echo "manual" > /sys/class/drm/card1/device/power_dpm_force_performance_level \
   && log "Successfully enabled manual mode." \
   || log "Failed to enable manual mode."
 
-log "Enabling the fan, and setting 8-bit pwm value of fan."
+log "Enabling, and setting the 8-bit pwm value of the fan."
 echo "1" > /sys/class/drm/card1/device/hwmon/hwmon2/fan1_enable
-check_status "enabled" "enable" "the fan."
+check_status "enabled " "enable " "the fan."
 echo "40" > /sys/class/drm/card1/device/hwmon/hwmon2/pwm1
 check_status "" "" "set fan pwm value."
 
@@ -37,11 +38,11 @@ check_status "" "" "set mclk:1 to 1156mhz."
 
 log "Applying the new sclk:0 and mclk:1 values"
 echo "c" > /sys/class/drm/card1/device/pp_od_clk_voltage
-check_status "applied" "apply" "sclk:0 and mclk:1."
+check_status "applied " "apply " "sclk:0 and mclk:1."
 
 log "Enabling only the max memory state."
 echo "3" > /sys/class/drm/card1/device/pp_dpm_mclk
-check_status "enabled" "enable" "only the max memory state."
+check_status "enabled " "enable " "only the max memory state."
 
 log "Setting the power mode to custom."
 echo "6" > /sys/class/drm/card1/device/pp_power_profile_mode
