c26a36b83e5e2264bcf087cff3f03ee9e00e6fa4
mellw0101 <melwivensson@gmail.com>
2025-04-20 19:38:45 +0200

    No msg

--- amdgpu_pp_service.sh
+++ amdgpu_pp_service.sh
@@ -20,9 +20,8 @@ check_status() {
 sleep 1
 
 log "Enabling manual mode."
-echo "manual" > /sys/class/drm/card1/device/power_dpm_force_performance_level \
-  && log "Successfully enabled manual mode." \
-  || log "Failed to enable manual mode."
+echo "manual" > /sys/class/drm/card1/device/power_dpm_force_performance_level 
+check_status "enabled " "enable " "manual mode."
 
 log "Enabling, and setting the 8-bit pwm value of the fan."
 echo "1" > /sys/class/drm/card1/device/hwmon/hwmon2/fan1_enable
@@ -36,7 +35,7 @@ check_status "" "" "set sclk:0 to 1600mhz."
 echo "m 1 1156" > /sys/class/drm/card1/device/pp_od_clk_voltage
 check_status "" "" "set mclk:1 to 1156mhz."
 
-log "Applying the new sclk:0 and mclk:1 values"
+log "Applying the new sclk:0 and mclk:1 values."
 echo "c" > /sys/class/drm/card1/device/pp_od_clk_voltage
 check_status "applied " "apply " "sclk:0 and mclk:1."
 
