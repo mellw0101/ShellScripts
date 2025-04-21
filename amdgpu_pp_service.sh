92196d0701cbdd7116aee2ed278b91074725b6c2
mellw0101 <melwivensson@gmail.com>
2025-04-21 21:14:34 +0200

    No msg

--- amdgpu_pp_service.sh
+++ amdgpu_pp_service.sh
@@ -17,17 +17,20 @@ check_status() {
   fi
 }
 
+FAN_BITVAL=40
+FAN_PROCENT=$(awk "BEGIN { printf \"%.2f\", (($FAN_BITVAL / 255) * 100) }")
+
 sleep 1
 
 log "Enabling manual mode."
 echo "manual" > /sys/class/drm/card1/device/power_dpm_force_performance_level 
 check_status "enabled " "enable " "manual mode."
 
-log "Enabling, and setting the 8-bit pwm value of the fan."
+log "Enabling, and setting the 8-bit pwm value of the fan to $FAN_BITVAL ('$FAN_PROCENT%')."
 echo "1" > /sys/class/drm/card1/device/hwmon/hwmon2/fan1_enable
 check_status "enabled " "enable " "the fan."
-echo "40" > /sys/class/drm/card1/device/hwmon/hwmon2/pwm1
-check_status "" "" "set fan pwm value."
+echo "$FAN_BITVAL" > /sys/class/drm/card1/device/hwmon/hwmon2/pwm1
+check_status "" "" "set fan pwm value to $FAN_BITVAL."
 
 log "Setting sclk:0:1600mhz mclk:1:1156mhz."
 echo "s 0 1600" > /sys/class/drm/card1/device/pp_od_clk_voltage
