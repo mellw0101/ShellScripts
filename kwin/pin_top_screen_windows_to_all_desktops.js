afa9ce0262cf19b72dc22644d783d1c6a0f6eb6c
mellw0101 <melwivensson@gmail.com>
2025-06-14 13:31:45 +0200

    No msg

---
+++ kwin/pin_top_screen_windows_to_all_desktops.js
@@ -0,0 +1,28 @@
+
+// Top monitor size
+var TOP_MONITOR_WIDTH = 3840;
+var TOP_MONITOR_HEIGHT = 1200;
+
+function find_top_monitor_id_by_size() {
+  for (var i=0; i<workspace.numScreens; ++i) {
+    var geom = workspace.screenGeometry(i);
+    if (geom.width === TOP_MONITOR_WIDTH && geom.height === TOP_MONITOR_HEIGHT) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+var top_monitor_id = find_top_monitor_id_by_size();
+
+workspace.windowAdded.connect(function(client) {
+  setTimeout(function() {
+    if (top_monitor_id === -1) {
+      return;
+    }
+    var window_screen = workspace.screenNumber(client.geometry.center());
+    if (window_screen === top_monitor_id) {
+      client.onAllDesktops = true;
+    }
+  }, 100);
+});
